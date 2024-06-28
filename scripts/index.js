adfec7d4d9498d78e6b3f184d35bfa0f46fe640a
Stepan-Kukhnikov <115821625+Stepan-Kukhnikov@users.noreply.github.com>
2024-06-28 15:33:39 +0300

    1st commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,41 @@
 // @todo: Темплейт карточки
 
+let cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
+let placesList = document.querySelector('.places__list');
+
+function renderCard(arrayEl, template) {
+
+    arrayEl.forEach((element) => {
+
+        let localCard = template.querySelector('.places__item').cloneNode(true);
+        let cardImage = localCard.querySelector('.card__image');
+        let cardtext = localCard.querySelector('.card__title');
+
+        cardImage.src = element.link;
+        cardtext.textContent = element.name;
+        placesList.append(localCard);
+    }) 
+    deleteCard(); 
+}
+
 
 // @todo: Функция удаления карточки
 
+function deleteCard() {
+    let cardDeleteButton = document.querySelector('.card__delete-button');
+    cardDeleteButton.addEventListener('click', function() {
+        const cardsItem = cardDeleteButton.closest('.places__item');
+        cardsItem.remove();
+    }
+    )
+}
+
+// К сожалению кнопка удаления работает только на первую карточку из списка
+// много разных вариантов перепробовал, на данный момент идеи закончились, не подскажите, что я делаю не так?
+
 // @todo: Вывести карточки на страницу
+
+renderCard(initialCards, cardTemplate);
