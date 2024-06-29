32ed0e1d089a1da0adfca978389c5b689a16cf3c
Stepan-Kukhnikov <115821625+Stepan-Kukhnikov@users.noreply.github.com>
2024-06-29 22:12:50 +0300

    add alt to images

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Проектная работа Mesto
 
-Проектная работа "Место" - https://github.com/Stepan-Kukhnikov/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,15 @@ const template = document.querySelector('#card-template').content;
 
 const placesList = document.querySelector('.places__list');
 
-function createCard(cardLink, cardName, cardTemplate){
+function createCard(cardLink, cardName, cardAlternative, cardTemplate){
 
-        let cardItem = cardTemplate.querySelector('.card').cloneNode(true);
-        let cardImage = cardItem.querySelector('.card__image');
-        let cardtext = cardItem.querySelector('.card__title');
+        const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
+        const cardImage = cardItem.querySelector('.card__image');
+        const cardtext = cardItem.querySelector('.card__title');
         const cardDeleteButton = cardItem.querySelector('.card__delete-button');
 
         cardImage.src = cardLink;
+        cardImage.alt = cardAlternative;
         cardtext.textContent = cardName;
         cardDeleteButton.addEventListener('click', event => deleteCard(event.target));
         return cardItem;
@@ -17,7 +18,7 @@ function createCard(cardLink, cardName, cardTemplate){
 
 function renderCards(cardEl) {
     cardEl.forEach((element) => {
-        placesList.append(createCard(element.link, element.name, template));
+        placesList.append(createCard(element.link, element.name, element.alternative, template));
     })
 }
 
