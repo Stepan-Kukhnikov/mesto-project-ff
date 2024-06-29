5e27d32fe351e4a003a8802f14f0af20679ae0a8
Stepan-Kukhnikov <115821625+Stepan-Kukhnikov@users.noreply.github.com>
2024-06-29 21:54:52 +0300

    second commit

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Проектная работа Mesto
 
-Проект "Место" - https://github.com/Stepan-Kukhnikov/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,28 @@
-// @todo: Темплейт карточки
+const template = document.querySelector('#card-template').content;
 
-let cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function createCard(cardLink, cardName, cardTemplate){
 
-let placesList = document.querySelector('.places__list');
+        let cardItem = cardTemplate.querySelector('.card').cloneNode(true);
+        let cardImage = cardItem.querySelector('.card__image');
+        let cardtext = cardItem.querySelector('.card__title');
+        const cardDeleteButton = cardItem.querySelector('.card__delete-button');
 
-function renderCard(arrayEl, template) {
-
-    arrayEl.forEach((element) => {
-
-        let localCard = template.querySelector('.places__item').cloneNode(true);
-        let cardImage = localCard.querySelector('.card__image');
-        let cardtext = localCard.querySelector('.card__title');
-
-        cardImage.src = element.link;
-        cardtext.textContent = element.name;
-        placesList.append(localCard);
-    }) 
-    deleteCard(); 
+        cardImage.src = cardLink;
+        cardtext.textContent = cardName;
+        cardDeleteButton.addEventListener('click', event => deleteCard(event.target));
+        return cardItem;
 }
 
-
-// @todo: Функция удаления карточки
-
-function deleteCard() {
-    let cardDeleteButton = document.querySelector('.card__delete-button');
-    cardDeleteButton.addEventListener('click', function() {
-        const cardsItem = cardDeleteButton.closest('.places__item');
-        cardsItem.remove();
-    }
-    )
+function renderCards(cardEl) {
+    cardEl.forEach((element) => {
+        placesList.append(createCard(element.link, element.name, template));
+    })
 }
 
-// К сожалению кнопка удаления работает только на первую карточку из списка
-// много разных вариантов перепробовал, на данный момент идеи закончились, не подскажите, что я делаю не так?
+function deleteCard(target) {
+    target.closest('.card').remove()
+}
 
-// @todo: Вывести карточки на страницу
-
-renderCard(initialCards, cardTemplate);
+renderCards(initialCards);
